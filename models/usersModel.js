abcd589e04daa61c07c254b20861ab538a4824f9
Maria <mary-sberbank@yandex.ru>
2022-09-09 15:52:27 +0300

    add userSchema

--- models/usersModel.js
+++ models/usersModel.js
@@ -4,48 +4,47 @@ const bcrypt = require('bcryptjs');
 const UnauthorizedError = require('../errors/unauthorized-error');
 
 const userSchema = new mongoose.Schema({
-    email: {
-        validate: {
-          validator(email) {
-            return validator.isEmail(email);
-          },
-          message: '',
-        },
-        type: String,
-        required: true,
-        unique: true,
+  email: {
+    validate: {
+      validator(email) {
+        return validator.isEmail(email);
       },
-      password: {
-        type: String,
-        required: true,
-        select: false,
-      },
-      name: {
-        required: true,
-        type: String,
-        minlength: 2,
-        maxlength: 30,
-      },
+      message: '',
+    },
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  name: {
+    required: true,
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+  },
 });
 
 userSchema.statics.findUserByCredentials = function fn({ email, password }) {
-    return this.findOne({ email })
-      .select('+password')
-      .then((user) => {
-        if (!user) {
-          throw new UnauthorizedError('Передан неверный логин или пароль');
-        }
-  
-        return bcrypt.compare(password, user.password)
-          .then((matched) => {
-            if (!matched) {
-              throw new UnauthorizedError('Передан неверный логин или пароль');
-            }
-  
-            return user;
-          });
-      });
-  };
+  return this.findOne({ email })
+    .select('+password')
+    .then((user) => {
+      if (!user) {
+        throw new UnauthorizedError('Передан неверный логин или пароль');
+      }
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            throw new UnauthorizedError('Передан неверный логин или пароль');
+          }
+
+          return user;
+        });
+    });
+};
   
 
 module.exports = mongoose.model('user', userSchema);
