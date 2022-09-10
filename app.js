f293cc81f7ffcb86a61c0c7d45c4f81c9f56ab55
Maria <mary-sberbank@yandex.ru>
2022-09-10 12:02:49 +0300

    add helmet & limiter

--- app.js
+++ app.js
@@ -2,15 +2,18 @@ require('dotenv').config();
 
 const mongoose = require('mongoose');
 const express = require('express');
+const helmet = require('helmet');
 const cors = require('cors');
 const { errors } = require('celebrate');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const error = require('./middlewares/error');
+const limiter = require('./middlewares/limiter');
 const router = require('./routes/index');
 
 const { MONGO_URL, PORT } = require('./helpers/config');
 
 const app = express();
+app.use(helmet());
 app.use(cors());
 
 app.use(express.json());
@@ -18,6 +21,9 @@ app.use(express.urlencoded({ extended: true }));
 
 // логгер запросов
 app.use(requestLogger);
+
+// лимиттер запросов
+app.use(limiter);
 
 // все роуты
 app.use(router);
